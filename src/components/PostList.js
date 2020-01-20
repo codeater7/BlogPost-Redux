3355a66c304300c4e19483444994ffe9dda84260
Sujan <pokharelsujan134@gmail.com>
2020-01-20 19:40:31 +0200

    before error

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,42 +1,39 @@
-import React from 'react'
-import {connect } from 'react-redux';
-import {fetchPosts } from '../actions'
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
-    componentDidMount(){
-        this.props.fetchPosts();
-    }
-    renderList(){
-        return this.props.posts.map(post =>{
-            return (
-                <div className="item " >
-                    <i className= "large middle aligned icon user"/>
-                    <div className="content">
-                        <div className="description">
-                            
-                            <h2>{post.title}</h2> 
-                            <p>{post.body}</p>
-                        </div>
-
-                    </div>
-
-                </div>
-            )
-        })
-    }
-    // mathi ko post.title ra body is from api
-    render (){
-        console.log(this.props.posts)
-        return (<div className="ui relaxed divided list">list </div>)
-    }
+	componentDidMount() {
+		this.props.fetchPosts();
+	}
+	renderList() {
+		return this.props.posts.map(post => {
+			return (
+				<div className="item ">
+					<i className="large middle aligned icon user" />
+					<div className="content">
+						<div className="description">
+							<h2>{post.title}</h2>
+							<p>{post.body}</p>
+						</div>
+					</div>
+				</div>
+			);
+		});
+	}
+	// mathi ko post.title ra body is from api
+	render() {
+		console.log(this.props.fetchPosts());
+		return <div className="ui relaxed divided list">{} </div>;
+	}
 }
 //this state will hold posts that our reducer has returned
 
-const mapStateToProps =(state)=>{
-    return { posts: state.posts}
+const mapStateToProps = state => {
+	return { posts: state.posts };
+};
 
-}
-
-
-
-export default  connect(mapStateToProps, {fetchPosts:fetchPosts})(PostList);
+export default connect(
+	mapStateToProps,
+	{ fetchPosts: fetchPosts }
+)(PostList);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-import App from './components/App'
-import reducers from './reducers'
+import App from './components/App';
+import reducers from './reducers';
 
 //Hooking middleware to redux store
-const Store = createStore(reducers, applyMiddleware(thunk))
+const Store = createStore(reducers, applyMiddleware(thunk));
 
-ReactDOM.render( 
-<Provider store= {Store}>
- <App/> 
- </Provider>, 
- document.querySelector("#root")
- );
+ReactDOM.render(
+	<Provider store={Store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
